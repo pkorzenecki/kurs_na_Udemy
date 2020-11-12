9d3017ba3e6c437d70adaf644613a1839a681650
pkorzenecki <59505026+pkorzenecki@users.noreply.github.com>
2020-11-12 18:03:42 +0100

    Commit po 5 sekcji - zadań i kursu

---
+++ src/zadaniaDoKursu_cz5/InputCalculator.java
@@ -0,0 +1,36 @@
+package zadaniaDoKursu_cz5;
+
+import java.util.Scanner;
+
+public class InputCalculator {
+    public static void main(String[] args) {
+        inputThenPrintSumAndAverage();
+    }
+
+    public static void inputThenPrintSumAndAverage() {
+        Scanner scanner = new Scanner(System.in);
+
+
+        int sum = 0;
+        int count = 0;
+        double average = 0;
+
+        while (true) {
+            boolean hasNextInt = scanner.hasNextInt();
+
+            if (hasNextInt) {
+                int number = scanner.nextInt();
+                count++;
+
+                sum += number;
+                average = (double) sum / count;
+            } else {
+                break;
+            }
+            scanner.nextLine();
+        }
+        System.out.println("SUM = " + sum + " AVG = " + Math.round(average));
+
+        scanner.close();
+    }
+}
---
+++ src/zadaniaDoKursu_cz5/LargestPrime.java
@@ -0,0 +1,24 @@
+package zadaniaDoKursu_cz5;
+
+public class LargestPrime {
+    public static void main(String[] args) {
+        System.out.println(getLargestPrime(21));
+    }
+
+    public static int getLargestPrime(int number) {
+        if (number < 2) {
+            return -1;
+        } else {
+            int max = 2;
+            for (int i = 2; i <= number; i++) {
+                while (number % i == 0) {
+                    number /= i;
+                    if (max < i) {
+                        max = i;
+                    }
+                }
+            }
+            return max;
+        }
+    }
+}
